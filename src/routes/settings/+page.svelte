d11995419f0f05f4d02431498e0448a6e07ccf0a
Millo Evers <contact@milloevers.com>
2023-03-17 12:33:56 +0000

    First update to settings page

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
     import { wipeDb, wipeUsers } from '$lib/db';
     import { Settings, updateSettings } from '$lib/stores';
-    import { TabGroup, Tab } from '@skeletonlabs/skeleton';
+    import { AccordionGroup, AccordionItem } from '@skeletonlabs/skeleton';
     import { writable } from 'svelte/store';
     import { store } from '../../hooks/auth';
-
-    let storeThree = writable('a');
 
     function saveSettings() {
         updateSettings([
@@ -23,47 +21,40 @@
 </script>
 
 <div class="container mx-auto p-8 space-y-8">
+    <h2>Library Settings</h2>
     <div class="card card-body space-y-4">
-        <TabGroup selected={storeThree} justify="justify-start md:justify-center" borderColor="border-tertiary-500" fill="fill-tertiary-500" color="text-tertiary-500" hover="hover:bg-tertiary-500/10">
+        <!-- <TabGroup selected={storeThree} justify="justify-start md:justify-center" borderColor="border-tertiary-500" fill="fill-tertiary-500" color="text-tertiary-500" hover="hover:bg-tertiary-500/10">
             <Tab value="a">
                 <svelte:fragment slot="lead">
-                    <!-- <SvgIcon name="book" fill="fill-inherit" class="!block" /> -->
                 </svelte:fragment>
                 General
             </Tab>
             <Tab value="b">
                 <svelte:fragment slot="lead">
-                    <!-- <SvgIcon name="clapperboard" fill="fill-inherit" class="!block" /> -->
                 </svelte:fragment>
                 Theme
             </Tab>
             <Tab value="c">
                 <svelte:fragment slot="lead">
-                    <!-- <SvgIcon name="tv" fill="fill-inherit" class="!block" /> -->
                 </svelte:fragment>
                 Other
             </Tab>
-        </TabGroup>
-        <div>
-            {#if $storeThree === 'a'}
-                <div class="container mx-auto p-8 space-y-8">
-                    <div class="card card-body space-y-4">
-                        Library Name <input id="libName">
-                        <button class="btn variant-filled-primary btn-base" on:click={()=>{saveSettings()}}>Save</button>
-                        <br/>
-                        <button class="btn variant-filled-primary" on:click={()=>{wipeDb()}}>Delete All Books</button>
-                        <button class="btn variant-filled-primary" on:click={()=>{wipeUsers()}}>Delete All Users</button>
-                    </div>
-                </div>
-            {/if}
-            {#if $storeThree === 'b'}
-                <h3>Themes</h3>
-                <p>Nisl nunc mi ipsum faucibus vitae aliquet nec. Ac ut consequat semper viverra nam libero justo laoreet. Nec sagittis aliquam malesuada.</p>
-            {/if}
-            {#if $storeThree === 'c'}
-                <h3>Television</h3>
-                <p>Ut sem viverra aliquet eget sit. Porttitor lacus luctus accumsan tortor posuere ac ut consequat. Vulputate enim nulla aliquet porttitor.</p>
-            {/if}
+        </TabGroup> -->
+        <div class="container mx-auto p-8 space-y-8">
+            <div class="card card-body space-y-4">
+                Library Name <input id="libName">
+                <button class="btn variant-filled-primary btn-base" on:click={()=>{saveSettings()}}>Save</button>
+                <br/>
+                <AccordionGroup>
+                    <AccordionItem>
+                        <svelte:fragment slot="summary"><p class="font-bold">Advanced</p></svelte:fragment>
+                        <svelte:fragment slot="content">
+                            <button class="btn variant-filled-primary" on:click={()=>{wipeDb()}}>Delete All Books</button>
+                            <button class="btn variant-filled-primary" on:click={()=>{wipeUsers()}}>Delete All Users</button>
+                        </svelte:fragment>
+                    </AccordionItem>
+                </AccordionGroup>
+            </div>
         </div>
     </div>
 </div>
